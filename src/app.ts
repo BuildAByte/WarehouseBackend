62e59e9534c3fa75a2a7a4caf5845a6cb1a2e11a
William Pfaffe <william@hedia.co>
2023-05-06 23:07:34 +0200

    add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
 import { default as express } from "express";
 import Picking from "./routes/picking.js";
 import Worker from "./routes/worker.js";
